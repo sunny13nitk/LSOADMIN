eb5fd95bf6680054873cebebf477e814ce3ef95c
Sunny Bhardwaj <sunny13nitk@gmail.com>
2023-08-19 13:13:58 +0530

    local test

--- srv/src/main/java/com/sap/cds/lsoadmin/config/AppSecurityConfig.java
+++ srv/src/main/java/com/sap/cds/lsoadmin/config/AppSecurityConfig.java
@@ -30,9 +30,6 @@ public class AppSecurityConfig
   // @Autowired
   // private XsuaaServiceConfiguration xsuaaServiceConfiguration;
   // @Autowired
-  // private XsuaaServiceConfiguration xsuaaServiceConfiguration;
-
-  // @Autowired
   // XsuaaTokenFlows xsuaaTokenFlows;
 
   @Bean 
@@ -42,36 +39,36 @@ public class AppSecurityConfig
     /*
      * ----------- Local Testing --------------------
      */
-    // return
-    // http.requestMatchers().antMatchers("/api/**").antMatchers("/esslocal/**").antMatchers("/poclocal/**").and()
-    // .csrf().disable() // don't insist on csrf tokens in put, post etc.
-    // .authorizeRequests().anyRequest().permitAll().and().build();
+    return
+    http.requestMatchers().antMatchers("/cockpit/**").antMatchers("/admin/**").and()
+    .csrf().disable() // don't insist on csrf tokens in put, post etc.
+    .authorizeRequests().anyRequest().permitAll().and().build();
 
     /*
      * ----------- CF Deployment --------------------
      */
-    // @formatter:off
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        // session is created by approuter
-        .and().authorizeRequests() // authorize all requests
-        .antMatchers("/admin/**").hasAuthority("Administrators").antMatchers("/cockpit/**").authenticated()
-        .antMatchers("/api/**").authenticated().anyRequest().denyAll().and().oauth2ResourceServer()
-        .bearerTokenResolver(new IasXsuaaExchangeBroker(xsuaaTokenFlows)).jwt()
-        .jwtAuthenticationConverter(getJwtAuthoritiesConverter());
-    // @formatter:on
+    // // @formatter:off
+    // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    //     // session is created by approuter
+    //     .and().authorizeRequests() // authorize all requests
+    //     .antMatchers("/admin/**").hasAuthority("Administrators").antMatchers("/cockpit/**").authenticated()
+    //     .antMatchers("/api/**").authenticated().anyRequest().denyAll().and().oauth2ResourceServer()
+    //     .bearerTokenResolver(new IasXsuaaExchangeBroker(xsuaaTokenFlows)).jwt()
+    //     .jwtAuthenticationConverter(getJwtAuthoritiesConverter());
+    // // @formatter:on
 
-    // return http.build();
+    // // return http.build();
 
   }
 
   // /*
   // ----------- CF Deployment --------------------
   // */
-  Converter<Jwt, AbstractAuthenticationToken> getJwtAuthoritiesConverter()
-  {
-    TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
-    converter.setLocalScopeAsAuthorities(true);
-    return converter;
-  }
+  // Converter<Jwt, AbstractAuthenticationToken> getJwtAuthoritiesConverter()
+  // {
+  //   TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
+  //   converter.setLocalScopeAsAuthorities(true);
+  //   return converter;
+  // }
 
 }
